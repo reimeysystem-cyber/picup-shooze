991bfd9ef2ea6adf74e3eba8e2964fcbb2ff404e
reimeysystem-cyber <reimeysystem@gmail.com>
2026-05-11 17:29:54 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKj_r2QHn47KgQXVQmg8_j4_mm3HHzZBY",
+  authDomain: "my-shooze-picup.firebaseapp.com",
+  projectId: "my-shooze-picup",
+  storageBucket: "my-shooze-picup.firebasestorage.app",
+  messagingSenderId: "701673239363",
+  appId: "1:701673239363:web:2cbdf76aac16be677ab5f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
